e4bda2e122fbc25ce2a67f1405c1942902a582c3
vinu0404 <22cd3034@rgipt.ac.in>
2025-02-18 21:07:25 +0530

    Add Config

---
+++ config.py
@@ -0,0 +1,8 @@
+EPOCHS = 10
+BATCH_SIZE = 32
+CLASSES = ["ApplyEyeMakeup", "ApplyLipstick", "Archery"]
+DATASET_PATH = "dataset"
+MODEL_PATH = "model.h5" 
+VIDEO_SOURCE = "test_videos/vid.avi"
+RTSP_STREAM = "rtsp://localhost:8554/stream"
+RESULTS_STREAM = "rtsp://localhost:8555/results"
